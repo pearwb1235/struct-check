f006339461bd4415660be6e63e4cc8f267ad55bc
梨水竹 <46672356+pearwb1235@users.noreply.github.com>
2021-11-06 16:18:11 +0800

    fix: d.ts import alia path

--- dist/functions/arrayStruct.d.ts
+++ dist/functions/arrayStruct.d.ts
@@ -1,3 +1,3 @@
-import { CheckFunction, Rule } from "~/checker";
-import { Enumerable } from "~/type";
+import { CheckFunction, Rule } from "../checker";
+import { Enumerable } from "../type";
 export declare function arrayStructChecker<T>(rules: Enumerable<Rule<T>>, require?: boolean): CheckFunction<T[]>;
--- dist/functions/switchStruct.d.ts
+++ dist/functions/switchStruct.d.ts
@@ -1,5 +1,5 @@
-import { CheckFunction, Rule } from "~/checker";
-import { Enumerable } from "~/type";
+import { CheckFunction, Rule } from "../checker";
+import { Enumerable } from "../type";
 export declare function switchStructChecker<T, U extends string>(switchFn: (value: T) => U, rules: {
     [K in U]: Enumerable<Rule<T>>;
 }): CheckFunction<T>;
